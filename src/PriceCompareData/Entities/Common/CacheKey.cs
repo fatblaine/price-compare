474cced5a27a594e94d4bfe25930bd92316661cc
fan <969103266@qq.com>
2025-08-24 21:22:08 +1000

    fix: add cache

---
+++ src/PriceCompareData/Entities/Common/CacheKey.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceCompareData.Entities.Common
+{
+    public class CacheKey
+    {
+        public const string COLES_DOWNDOWN_PRODUCTS = "ColesDownDownProducts";
+    }
+}
