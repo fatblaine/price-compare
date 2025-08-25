03e6ccc3cfad484db8a1b51faaab6a858e6a1019
fan <969103266@qq.com>
2025-08-25 15:19:00 +1000

    modify: add multi-conditions search, fix redis problems

--- src/PriceCompareWeb/obj/Debug/net9.0/PriceCompareWeb.AssemblyInfo.cs
+++ src/PriceCompareWeb/obj/Debug/net9.0/PriceCompareWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PriceCompareWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710df88dc2107c56ed0d976fc06e20522185600c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb4082df421dfb9dd271b823eddd2e70ad9b444")]
 [assembly: System.Reflection.AssemblyProductAttribute("PriceCompareWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PriceCompareWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
