a250c8a9f57fa4897ea7c0ec59ce4ab886610f18
fan <969103266@qq.com>
2025-08-26 17:23:11 +1000

    fix: register Quartz

--- src/PriceCompareWeb/Program.cs
+++ src/PriceCompareWeb/Program.cs
@@ -35,7 +35,7 @@ builder.Services.AddQuartz(q =>
     q.AddTrigger(opts => opts
         .ForJob(jobKey)
         .WithIdentity("ColesRefreshJob-trigger")
-        .WithCronSchedule("0 0 16 * * ?"));
+        .WithCronSchedule("0 0 6 * * ?"));
 });
 
 builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
