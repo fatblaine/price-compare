071b9806a50c697110a6d430fe604acaef39f083
fan <969103266@qq.com>
2025-09-01 22:49:41 +1000

    modify: use internal API to scrape on-special products of Coles

---
+++ src/PriceCompareData/DTOs/ImageUri.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceCompareData.DTOs
+{
+    public class ImageUri
+    {
+        public string AltText { get; set; }
+        public string Type { get; set; }
+        public string Uri { get; set; }
+    }
+}
