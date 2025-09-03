828efa19991dc2571ae315014a2eabef498e1006
fan <969103266@qq.com>
2025-09-03 22:41:09 +1000

    BTS-7 Add filter to GetAllOnSpecialProductsAsync API

--- src/PriceCompareData/Entities/Scraping/ColesSpecialProduct.cs
+++ src/PriceCompareData/Entities/Scraping/ColesSpecialProduct.cs
@@ -13,14 +13,13 @@ namespace PriceCompareData.Entities
         public decimal? OriginalPrice { get; set; }
         public string PricePerUnit { get; set; }
         public string ImageUrl { get; set; }
-        public string ProductUrl { get; set; }
-        public string WasPriceText { get; set; }
+        // public string ProductUrl { get; set; }
+        // public string WasPriceText { get; set; }
         public bool IsSponsored { get; set; }
         // The amount saved compared to the original price
         // public string Save { get; set; }
         // // rating
         // public string Rating { get; set; }
-
         public DateTime ScrapedAt { get; set; }
     }
 }
