41b62aa1933e6966fe91313b44324c3a97fc1c33
fan <969103266@qq.com>
2025-09-11 22:29:58 +1000

    [BTS-10] [BTS-16] Add Category, CategoryKeyword, and Product entities with seeding and indexes

---
+++ src/PriceCompareData/Entities/Compare/CategoryKeyword.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceCompareData.Entities.Compare
+{
+    public class CategoryKeyword
+    {
+        public int KeywordId { get; set; }
+        public int CategoryId { get; set; }
+        public string Keyword { get; set; } = default!;
+        public int Weight { get; set; } = 1;
+    }
+}
