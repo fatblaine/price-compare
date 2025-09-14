6723c33d687f1e1366987924263150ce6b44b85e
fan <969103266@qq.com>
2025-09-14 16:25:01 +1000

    BTS-17: Modify the Jira key check rules.

--- src/PriceCompareData/Entities/Compare/CategoryKeyword.cs
+++ src/PriceCompareData/Entities/Compare/CategoryKeyword.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace PriceCompareData.Entities.Compare
 {
     public class CategoryKeyword
     {
+        [Key]
         public int KeywordId { get; set; }
         public int CategoryId { get; set; }
         public string Keyword { get; set; } = default!;
