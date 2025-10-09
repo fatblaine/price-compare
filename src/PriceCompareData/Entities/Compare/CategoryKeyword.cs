8b2064c90586c65b9585ac72907322bd61a62479
fan <969103266@qq.com>
2025-10-09 12:57:32 +1100

    BTS-37 BTS-38 BTS-39 BTS-40: Build a Console Tool for Generating and Importing Category Keywords

--- src/PriceCompareData/Entities/Compare/CategoryKeyword.cs
+++ src/PriceCompareData/Entities/Compare/CategoryKeyword.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PriceCompareData.Entities.Compare
 {
+    [Table("CategoryKeyword")]
     public class CategoryKeyword
     {
         [Key]
+        [Column("keywordid")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int KeywordId { get; set; }
         public int CategoryId { get; set; }
         public string Keyword { get; set; } = default!;
