31bdefb8f339ea9ede7f413a3d463007e6eb2d7b
fan <969103266@qq.com>
2026-03-07 16:35:11 +1100

    BTS-120: Record every LLM call to a local file.

---
+++ src/PriceCompareCore/Config/LlmLogOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceCompareCore.Config
+{
+    public class LlmLogOptions
+    {
+        public string OutputDir { get; set; } = "llm-logs";
+    }
+}
