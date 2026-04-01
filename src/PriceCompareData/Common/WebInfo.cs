f140460409b13089c70e9ed53969ef02d15b0d03
fan <969103266@qq.com>
2026-04-01 14:26:17 +1100

    BTS-135: Added logging to FavoritePriceTrackingService.CheckAndNotifyAsync

--- src/PriceCompareData/Common/WebInfo.cs
+++ src/PriceCompareData/Common/WebInfo.cs
@@ -9,7 +9,7 @@ namespace PriceCompareData.Entities.Common
     {
         public const string COLES_BASE_URL = "https://www.coles.com.au";
         // Next.js build id used in Coles _next/data URLs. Update this when Coles deploys.
-        public const string COLES_NEXT_BUILD_ID = "20260317.3-bc041679f2cc8ef7d91d32a82700a32e18cddc26";
+        public const string COLES_NEXT_BUILD_ID = "20260326.1-e04350c8cd5bfce66ee7c1f052f5f5eaa36c0754";
         public const string COLES_NEXT_DATA_BASE = COLES_BASE_URL + "/_next/data/" + COLES_NEXT_BUILD_ID;
     }
 }
